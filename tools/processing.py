47d95bd7d47787ef8f929f8cbe13f827b26bedce
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2026-04-23 22:35:13 -0400

    Simplifying leading numbers removal

--- tools/processing.py
+++ tools/processing.py
@@ -1,3 +1,5 @@
+import re
+
 def find_index(roi_result, keyword):
     for idx, text in enumerate(roi_result):
         if keyword in text:
@@ -5,8 +7,4 @@ def find_index(roi_result, keyword):
     return -1
 
 def remove_leading_numbers(s):
-    for idx, char in enumerate(s):
-        if not char.isdigit():
-            i = idx
-            break
-    return s[i:].strip()
+    return re.sub(r'^\d+', '', s).strip()
